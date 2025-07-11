03bdb7fcc15e77594ada16f8fdebaea261d615a1
piitex <supermftw@gmail.com>
2025-07-11 17:58:29 -0500

    Added helper functions for theme color.

--- src/main/java/me/piitex/app/configuration/AppSettings.java
+++ src/main/java/me/piitex/app/configuration/AppSettings.java
@@ -97,21 +97,80 @@ public class AppSettings {
         }
     }
 
+    /*
+        Utility functions for getting theme coloring.
+        Needed for RichTextFX components
+     */
+
+    public String getThemeBorderColor(String theme) {
+        if (theme.equalsIgnoreCase("primer light")) {
+            return "#d0d7de";
+        } else if (theme.equalsIgnoreCase("nord light")) {
+            return "#c7ceda";
+        } else if (theme.equalsIgnoreCase("nord dark")) {
+            return "rgb(94, 102, 117)";
+        } else if (theme.equalsIgnoreCase("cupertino light")) {
+            return "rgb(209, 209, 214)";
+        } else if (theme.equalsIgnoreCase("cupertino dark")) {
+            return "rgb(72, 72, 74)";
+        } else if (theme.equalsIgnoreCase("dracula")) {
+            return "#685ab3";
+        } else {
+            return "#30363d";
+        }
+    }
+
+    public Color getThemeTextColor(String theme) {
+        if (theme.equalsIgnoreCase("primer light")) {
+            return Color.web("#24292f");
+        } else if (theme.equalsIgnoreCase("nord light")) {
+            return Color.web("#2E3440");
+        } else if (theme.equalsIgnoreCase("nord dark")) {
+            return Color.web("#ECEFF4");
+        } else if (theme.equalsIgnoreCase("cupertino light")) {
+            return Color.rgb(0, 0, 0);
+        } else if (theme.equalsIgnoreCase("cupertino dark")) {
+            return Color.rgb(255, 255, 255);
+        } else if (theme.equalsIgnoreCase("dracula")) {
+            return Color.web("#f8f8f2");
+        } else {
+            return Color.web("#c9d1d9");
+        }
+    }
+
     public String getThemeSubtleColor(String theme) {
         if (theme.equalsIgnoreCase("primer light")) {
-            return "#6e7781";
+            return "#f6f8fa";
         } else if (theme.equalsIgnoreCase("nord light")) {
             return "#eef1f5";
         } else if (theme.equalsIgnoreCase("nord dark")) {
             return "#3B4252";
         } else if (theme.equalsIgnoreCase("cupertino light")) {
-            return "242, 242, 247";
+            return "rgb(242, 242, 247)";
         } else if (theme.equalsIgnoreCase("cupertino dark")) {
-            return "44, 44, 46";
+            return "rgb(44, 44, 46)";
         } else if (theme.equalsIgnoreCase("dracula")) {
             return "#3d3f4a";
         } else {
             return "#161b22";
+        }
+    }
+
+    public String getThemeDefaultColor(String theme) {
+        if (theme.equalsIgnoreCase("primer light")) {
+            return "#ffffff";
+        } else if (theme.equalsIgnoreCase("nord light")) {
+            return "#fafafc";
+        } else if (theme.equalsIgnoreCase("nord dark")) {
+            return "#2E3440";
+        } else if (theme.equalsIgnoreCase("cupertino light")) {
+            return "rgb(255, 255, 255)";
+        } else if (theme.equalsIgnoreCase("cupertino dark")) {
+            return "rgb(28, 28, 30)";
+        } else if (theme.equalsIgnoreCase("dracula")) {
+            return "#282a36";
+        } else {
+            return "#0d1117";
         }
     }
 
