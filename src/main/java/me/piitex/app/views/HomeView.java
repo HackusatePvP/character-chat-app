bb82e0107c1797b78da92c7d1a6d4d9fe52d2310
piitex <supermftw@gmail.com>
2025-07-13 21:38:08 -0500

    Fixed typos with instructions.

--- src/main/java/me/piitex/app/views/HomeView.java
+++ src/main/java/me/piitex/app/views/HomeView.java
@@ -108,12 +108,12 @@ public class HomeView {
         body.add(new TextOverlay("3. Set a GPU. Ensure there is another option besides auto.\n\n"));
         body.add(new TextOverlay("4. Navigate to Models.\n\n"));
         body.add(new TextOverlay("5. Set the directory for your models.\n\n"));
-        body.add(new TextOverlay("6. Download a GGUF model from the \" Download \" tab."));
-        body.add(new TextOverlay("7. Set the model as a default (REQUIRED).\n\n"));
-        body.add(new TextOverlay("7. (Optional) Configure the model settings by clicking the blue gear.\n\n"));
-        body.add(new TextOverlay("8. Go back to settings and start the server.\n\n"));
-        body.add(new TextOverlay("9. (Optional) Create a User Template.\n\n"));
-        body.add(new TextOverlay("10. Create a character.\n"));
+        body.add(new TextOverlay("6. Download a GGUF model from the \"Download\" tab.\n\n"));
+        body.add(new TextOverlay("7. Set the model as a default in the \"List\" tab. (REQUIRED).\n\n"));
+        body.add(new TextOverlay("8. (Optional) Configure the model settings by clicking the blue gear.\n\n"));
+        body.add(new TextOverlay("9. Go back to settings and start the server.\n\n"));
+        body.add(new TextOverlay("10. (Optional) Create a User Template.\n\n"));
+        body.add(new TextOverlay("11. Create a character.\n"));
         bodyLayout.addElement(body);
 
         card.setBody(bodyLayout);
