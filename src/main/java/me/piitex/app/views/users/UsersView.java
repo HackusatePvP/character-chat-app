17e38535f0cb3b8c7284f6bb59f362bf5eb3e5c0
piitex <supermftw@gmail.com>
2025-08-20 13:44:51 -0500

    Started re-design of user templates.

--- src/main/java/me/piitex/app/views/users/UsersView.java
+++ src/main/java/me/piitex/app/views/users/UsersView.java
@@ -1,154 +1,176 @@
 package me.piitex.app.views.users;
 
 import atlantafx.base.theme.Styles;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
+import javafx.geometry.Side;
+import javafx.scene.Cursor;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
 import me.piitex.app.App;
 import me.piitex.app.backend.User;
-import me.piitex.app.views.HomeView;
+import me.piitex.app.configuration.AppSettings;
 import me.piitex.app.views.SidebarView;
 import me.piitex.engine.Container;
-import me.piitex.engine.PopupPosition;
 import me.piitex.engine.containers.CardContainer;
 import me.piitex.engine.containers.DialogueContainer;
 import me.piitex.engine.containers.EmptyContainer;
 import me.piitex.engine.containers.ScrollContainer;
+import me.piitex.engine.layouts.FlowLayout;
 import me.piitex.engine.layouts.HorizontalLayout;
 import me.piitex.engine.layouts.VerticalLayout;
-import me.piitex.engine.overlays.*;
+import me.piitex.engine.overlays.ButtonBuilder;
+import me.piitex.engine.overlays.ButtonOverlay;
+import me.piitex.engine.overlays.ImageOverlay;
+import me.piitex.engine.overlays.TextOverlay;
 import org.apache.commons.io.FileUtils;
 import org.kordamp.ikonli.javafx.FontIcon;
 import org.kordamp.ikonli.material2.Material2AL;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-public class UsersView {
-    private final Container container;
+public class UsersView extends EmptyContainer {
+    private final VerticalLayout layout;
+
+    private final AppSettings appSettings = App.getInstance().getAppSettings();
+
+    private int imageWidth, imageHeight, cardWidth, cardHeight;
 
     public UsersView() {
-        container = new EmptyContainer(1920, 1080);
+        super(App.getInstance().getAppSettings().getWidth(), App.getInstance().getAppSettings().getHeight());
 
-        HorizontalLayout root = new HorizontalLayout(1920, 1080);
+        HorizontalLayout root = new HorizontalLayout(0, 0);
         root.setSpacing(35);
-        container.addElement(root);
+        addElement(root);
 
         root.addElement(new SidebarView(root, false));
 
-        ButtonOverlay create = new ButtonBuilder("create").setText("Create User").build();
-        create.addStyle(Styles.SUCCESS);
-        create.addStyle(Styles.BUTTON_OUTLINED);
-        create.setX(900);
-        create.setY(20);
-        container.addElement(create);
-        create.onClick(event -> {
-            App.window.clearContainers();
-            App.window.addContainer(new UserEditView(null).getRoot());
+        layout = new VerticalLayout(getWidth(), getHeight());
+        layout.setAlignment(Pos.TOP_CENTER);
+        layout.setOffsetX(20);
+        layout.setSpacing(20);
+        root.addElement(layout);
+
+        init();
+    }
+
+    public void init() {
+        ButtonOverlay newUser = new ButtonBuilder("add").setText("New User").build();
+        newUser.addStyle(Styles.SUCCESS);
+        newUser.addStyle(Styles.BUTTON_OUTLINED);
+        layout.addElement(newUser);
+
+        ScrollContainer scrollContainer;
+        VerticalLayout base = new VerticalLayout(0, -1);
+        if (App.mobile) {
+            scrollContainer = new ScrollContainer(base, 0, 0, 400, -1);
+            scrollContainer.setMaxSize(400, 1000);
+            imageWidth = 128;
+            imageHeight = 128;
+            cardWidth = 160;
+            cardHeight = 250;
+            layout.setSpacing(70);
+        } else {
+            scrollContainer = new ScrollContainer(base, 10, 10, appSettings.getWidth() - 310, -1);
+            scrollContainer.setMaxSize(appSettings.getWidth() - 300, appSettings.getHeight() - 100);
+            imageWidth = 256;
+            imageHeight = 256;
+            cardWidth = 280;
+            cardHeight = 380;
+        }
+        scrollContainer.setScrollWhenNeeded(false);
+        scrollContainer.setHorizontalScroll(false);
+        scrollContainer.setVerticalScroll(true);
+
+
+        FlowLayout flowLayout = new FlowLayout(scrollContainer.getWidth(), -1);
+        base.addElement(flowLayout);
+
+        for (User user : App.getInstance().getUserTemplates().values()) {
+            flowLayout.addElement(createUserCard(user, flowLayout));
+        }
+    }
+
+    public CardContainer createUserCard(User user, FlowLayout base) {
+        CardContainer card = new CardContainer(0,0, cardWidth, cardHeight);
+        card.setMaxSize(cardWidth, cardHeight);
+
+        VerticalLayout displayBox = new VerticalLayout(0, 0);
+        displayBox.setAlignment(Pos.BASELINE_CENTER);
+
+        TextOverlay helper = new TextOverlay("Click to chat");
+        helper.setUnderline(true);
+        displayBox.addElement(helper);
+
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem edit = new MenuItem("Edit");
+        edit.setOnAction(event -> {
+            editUser(user);
+        });
+        MenuItem copy = new MenuItem("Copy");
+        copy.setOnAction(event -> {
+            duplicateUser(user);
+        });
+        MenuItem delete = new MenuItem("Delete");
+        delete.setOnAction(event -> {
+            deleteUser(user, base, card);
         });
 
-        root.addElement(build());
-    }
+        contextMenu.getItems().add(edit);
+        contextMenu.getItems().add(copy);
+        contextMenu.getItems().add(delete);
 
-    public ScrollContainer build() {
-        VerticalLayout layout = new VerticalLayout(0, 0);
-        layout.setSpacing(20);
-
-        ScrollContainer root = new ScrollContainer(layout, 20, 100, 1500, 0);
-        root.setMaxSize(1920, 900);
-        root.setVerticalScroll(true);
-        root.setScrollWhenNeeded(false);
-        root.setHorizontalScroll(false);
-
-        HorizontalLayout base = new HorizontalLayout(0, 0);
-        base.setSpacing(20);
-        layout.addElement(base);
-
-        int i = 0;
-        int max = (App.mobile ? 3 : 6);
-        for (User user : App.getInstance().getUserTemplates().values()) {
-            if (i == max) {
-                // Start a new horizontal row
-                base = new HorizontalLayout(0, 0);
-                base.setSpacing(20);
-                layout.addElement(base);
-                i = 0;
+        displayBox.setClickEvent(event -> {
+            if (event.getFxClick().getButton() == MouseButton.SECONDARY) {
+                if (contextMenu.isShowing()) return;
+                contextMenu.show(displayBox.getPane(), Side.BOTTOM, 60, 0);
             }
 
-            CardContainer card = new CardContainer(0, 0, 250, 380);
-            card.setMaxSize(250, 380);
+        });
 
-            VerticalLayout displayBox = new VerticalLayout(250, 300);
-            displayBox.setX(-10);
-            displayBox.setY(-10);
-
-            TextOverlay helper = new TextOverlay("Click to chat");
-            helper.setUnderline(true);
-            displayBox.addElement(helper);
-            displayBox.setAlignment(Pos.BASELINE_CENTER);
-
-            displayBox.setClickEvent(event -> {
-                // Load chat window...
-                MessageOverlay warning = new MessageOverlay(0, 0, 600, 100,"Warning", "This feature is still in development");
-                warning.addStyle(Styles.WARNING);
-                warning.addStyle(Styles.BG_DEFAULT);
-                App.window.renderPopup(warning, PopupPosition.BOTTOM_CENTER, 600, 100, false);
-            });
-
-            ImageOverlay icon = User.getUserAvatar(user.getIconPath(), 256, 256);
-            if (icon != null && icon.getImage() != null) {
-                displayBox.addElement(icon);
-            }
-            TextOverlay name = new TextOverlay(user.getId());
-            displayBox.addElement(name);
-
-            card.setBody(displayBox);
-
-            card.setFooter(buildControlBox(user));
-
-            base.addElement(card);
-
-            i++;
+        ImageOverlay icon = User.getUserAvatar(user.getIconPath(), imageWidth, imageHeight);
+        if (icon != null && icon.getImage() != null) {
+            icon.setPreserveRatio(false);
+            displayBox.addElement(icon);
         }
+        TextOverlay name = new TextOverlay(user.getId());
+        displayBox.addElement(name);
 
-        return root;
+        card.setBody(displayBox);
+
+        card.setFooter(buildControlBox(base, card, user));
+
+        return card;
     }
 
-    public HorizontalLayout buildControlBox(User user) {
+    public HorizontalLayout buildControlBox(FlowLayout base, CardContainer card, User user) {
         HorizontalLayout root = new HorizontalLayout(200, 25);
         root.setIndex(10);
         root.setSpacing(20);
-        root.setAlignment(Pos.BASELINE_CENTER);
+        if (!App.mobile) {
+            root.setAlignment(Pos.BASELINE_CENTER);
+        }
 
         FontIcon editIcon = new FontIcon(Material2AL.EDIT);
         editIcon.setIconSize(16);
         TextOverlay edit = new TextOverlay(editIcon);
-        edit.setTooltip("Edit the user");
+        edit.setTooltip("Edit the character");
+        edit.addStyle(Styles.ACCENT);
         edit.onClick(event -> {
-            UserEditView editView = new UserEditView(user);
-            App.window.clearContainers();
-            App.window.addContainer(editView.getRoot());
+            editUser(user);
         });
         root.addElement(edit);
 
         FontIcon duplicateIcon = new FontIcon(Material2AL.FILE_COPY);
         duplicateIcon.setIconSize(16);
         TextOverlay duplicate = new TextOverlay(duplicateIcon);
-        duplicate.setTooltip("Duplicate the user.");
+        duplicate.setTooltip("Duplicate the character.");
+        duplicate.addStyle(Styles.WARNING);
         duplicate.onClick(event -> {
-            // Duplicate the user.
-            String newId = user.getId() + " (copy)";
-            while (App.getInstance().getUser(newId) != null) {
-                newId += " (copy)";
-            }
-
-            // Edit the user in the edit view rather than duplicating the files
-            // Allow the id to be edited and changed.
-
-            // Create a copy of the user.
-            User duplicated = new User(newId);
-            duplicated.copy(user);
-
-            UserEditView editView = new UserEditView(duplicated, true);
-            App.window.clearContainers();
-            App.window.addContainer(editView.getRoot());
+            duplicateUser(user);
         });
         root.addElement(duplicate);
 
@@ -156,44 +178,101 @@ public class UsersView {
         TextOverlay delete = new TextOverlay(deleteIcon);
 
         delete.addStyle(Styles.DANGER);
-        delete.setTooltip("Delete the user.");
+        delete.setTooltip("Delete the character.");
         delete.onClick(event -> {
-            DialogueContainer dialogueContainer = new DialogueContainer("Delete '" + user.getId() + "'?", 500, 500);
-
-            ButtonOverlay cancel = new ButtonBuilder("cancel").setText("Keep").build();
-            cancel.setWidth(150);
-            cancel.addStyle(Styles.SUCCESS);
-            cancel.onClick(event1 -> App.window.removeContainer(dialogueContainer));
-
-            ButtonOverlay confirm = new ButtonBuilder("confirm").setText("Delete").build();
-            confirm.setWidth(150);
-            confirm.addStyle(Styles.DANGER);
-            confirm.onClick(event1 -> {
-                App.getInstance().getUserTemplates().remove(user.getId());
-                try {
-                    FileUtils.deleteDirectory(user.getUserDirectory());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-                App.window.clearContainers();
-                App.window.addContainer(new HomeView());
-            });
-
-            dialogueContainer.setCancelButton(cancel);
-            dialogueContainer.setConfirmButton(confirm);
-
-            // Render this on top
-            App.window.renderPopup(dialogueContainer, PopupPosition.CENTER, 500, 500);
-
+            deleteUser(base, card, user, event.getHandler().getSceneX(), event.getHandler().getSceneY());
         });
         root.addElement(delete);
-
-        // Add icons
         return root;
     }
 
-    public Container getRoot() {
-        return container;
+    private void editUser(User user) {
+        App.window.getStage().getScene().setCursor(Cursor.WAIT);
+
+        Container container;
+        if (App.mobile) {
+            // TODO: Open edit screen.
+        } else {
+            // TODO: Open edit screen.
+        }
+
+        Platform.runLater(() -> {
+            App.window.clearContainers();
+            //TODO: App.window.addContainer(container);
+            App.window.getStage().getScene().setCursor(Cursor.DEFAULT);
+        });
+    }
+
+    private void duplicateUser(User user) {
+        String newId = user.getId() + " (copy)";
+        while (App.getInstance().getUser(newId) != null) {
+            newId += " (copy)";
+        }
+
+        User duplicated = new User(newId, null);
+        duplicated.copy(user);
+
+//        CharacterEditView editView = new CharacterEditView(duplicated, true);
+//        App.window.clearContainers();
+//        App.window.addContainer(editView.getRoot());
+    }
+
+    private void deleteUser(FlowLayout base, CardContainer card, User user, double x, double y) {
+        DialogueContainer dialogueContainer = new DialogueContainer("Delete '" + user.getId() + "'?", 500, 500);
+
+        ButtonOverlay cancel = new ButtonBuilder("cancel").setText("Keep").build();
+        cancel.setWidth(150);
+        cancel.addStyle(Styles.SUCCESS);
+        cancel.onClick(event1 -> {
+            App.window.removeContainer(dialogueContainer);
+        });
+
+        ButtonOverlay confirm = new ButtonBuilder("confirm").setText("Delete").build();
+        confirm.setWidth(150);
+        confirm.addStyle(Styles.DANGER);
+        confirm.onClick(event1 -> {
+            App.getInstance().getUserTemplates().remove(user.getId());
+            App.window.removeContainer(dialogueContainer);
+
+            // Cleanup image usage
+            VerticalLayout verticalLayout = (VerticalLayout) card.getBody();
+            ImageOverlay imageOverlay = (ImageOverlay) verticalLayout.getElementAt(1);
+
+            // When setting to null the engine will dispose of the image and the JVM will call gc.
+            imageOverlay.setImage(null);
+
+            base.removeElement(card);
+
+            // Add a buffer to ensure image resources are disposed.
+            App.getThreadPoolManager().submitSchedule(() -> {
+                try {
+                    App.logger.info("Deleting: {}", user.getId());
+                    FileUtils.deleteDirectory(user.getUserDirectory());
+                } catch (IOException e) {
+                    App.logger.error("Could not delete directory!", e);
+                }
+            }, 1, TimeUnit.SECONDS);
+        });
+
+        dialogueContainer.setCancelButton(cancel);
+        dialogueContainer.setConfirmButton(confirm);
+
+        // Render this on top
+        App.window.renderPopup(dialogueContainer, x, y, 500, 500);
+    }
+
+    private void deleteUser(User user, FlowLayout base, CardContainer card) {
+        App.getInstance().getUserTemplates().remove(user.getId());
+
+        App.getThreadPoolManager().submitSchedule(() -> {
+            try {
+                App.logger.info("Deleting: {}", user.getId());
+                FileUtils.deleteDirectory(user.getUserDirectory());
+            } catch (IOException e) {
+                App.logger.error("Could not delete directory!", e);
+            }
+        }, 1, TimeUnit.SECONDS);
+
+        base.removeElement(card);
     }
 }
