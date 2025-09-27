cf9885ddc1ad8cb1f626b39a0e3c98080681ce3d
piitex <supermftw@gmail.com>
2025-09-27 07:12:41 -0500

    Added notes for run flag.

--- src/main/java/me/piitex/app/Main.java
+++ src/main/java/me/piitex/app/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static boolean run = false;
 
     public static void main(String[] args) {
+        // Sets a flag which indicates the program was launched through the jar file.
+        // When testing with the IDEA launch the application with JavaFXLoad.main()
         run = true;
         JavaFXLoad.main(args);
     }
