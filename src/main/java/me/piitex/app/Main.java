af0a77c5bf6d0ad8daee9ce0f5f8116757dfcbbc
piitex <supermftw@gmail.com>
2025-10-05 17:17:01 -0500

    Fixed jar launching issues.

--- src/main/java/me/piitex/app/Main.java
+++ src/main/java/me/piitex/app/Main.java
@@ -1,12 +1,15 @@
 package me.piitex.app;
 
+import javafx.application.Application;
+
 public class Main {
     public static boolean run = false;
 
     public static void main(String[] args) {
         // Sets a flag which indicates the program was launched through the jar file.
-        // When testing with the IDEA launch the application with JavaFXLoad.main()
+        // When testing with the IDEA launch the application with App.class
         run = true;
         new App();
+        Application.launch(App.class);
     }
 }
