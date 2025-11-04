75104fefbdfc0167a38b0c29b3db54e89bc4ae73
piitex <supermftw@gmail.com>
2025-11-04 02:11:08 -0600

    Added user id as a header.

--- src/main/java/me/piitex/app/views/users/UsersView.java
+++ src/main/java/me/piitex/app/views/users/UsersView.java
@@ -87,9 +87,9 @@ public class UsersView extends EmptyContainer {
             displayBox.setSpacing(15);
             displayBox.setAlignment(Pos.TOP_CENTER);
 
-            TextOverlay helper = new TextOverlay("Click to chat");
-            helper.setUnderline(true);
-            displayBox.addElement(helper);
+            TextOverlay id = new TextOverlay(user.getId());
+            displayBox.addElement(id);
+
 
             ContextMenu contextMenu = new ContextMenu();
 
@@ -109,7 +109,8 @@ public class UsersView extends EmptyContainer {
                 icon.setPreserveRatio(false);
                 displayBox.addElement(icon);
             }
-            TextOverlay name = new TextOverlay(user.getId());
+
+            TextOverlay name = new TextOverlay(user.getDisplayName());
             displayBox.addElement(name);
 
             card.setBody(displayBox);
