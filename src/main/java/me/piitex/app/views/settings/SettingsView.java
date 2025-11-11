d9160a0b871d2cc08fa777837d244318615c292b
piitex <supermftw@gmail.com>
2025-11-11 14:19:47 -0600

    Added window scaling setting.

--- src/main/java/me/piitex/app/views/settings/SettingsView.java
+++ src/main/java/me/piitex/app/views/settings/SettingsView.java
@@ -50,6 +50,7 @@ public class SettingsView {
         scrollContainer.setHorizontalScroll(false);
         root.addElement(scrollContainer);
 
+        layout.addElement(buildWindowScaling());
         layout.addElement(buildResolution());
         layout.addElement(buildGlobalChatSize());
         layout.addElement(buildChatSize());
@@ -57,6 +58,28 @@ public class SettingsView {
         layout.addElement(buildGeneralText());
         layout.addElement(buildQuotesColor());
         layout.addElement(buildAstrixColor());
+    }
+
+    public TileContainer buildWindowScaling() {
+        TileContainer tileContainer = new TileContainer(0, 0, appSettings.getWidth() - 300, 120);
+        tileContainer.setMaxSize(appSettings.getWidth() - 300, 120);
+        tileContainer.addStyle(Styles.BORDER_DEFAULT);
+        tileContainer.addStyle(Styles.BG_DEFAULT);
+        tileContainer.addStyle(appSettings.getGlobalTextSize());
+
+        tileContainer.setTitle("Window Scaling");
+        tileContainer.setDescription("Enable/Disable window scaling. Can cause text to become blurry or stretched.");
+
+        ToggleSwitchOverlay toggleSwitchOverlay = new ToggleSwitchOverlay(appSettings.isWindowScaling());
+        toggleSwitchOverlay.onToggle(event -> {
+            App.window.clear();
+            App.window.close(false);
+            appSettings.setWindowScaling(event.getNewValue());
+            App.getInstance().initialization(App.window.getStage());
+        });
+        tileContainer.setAction(toggleSwitchOverlay);
+
+        return tileContainer;
     }
 
     public TileContainer buildResolution() {
