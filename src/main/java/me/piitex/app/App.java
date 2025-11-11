6924a4e84411b73210af65420ed7cdf93b0f8019
piitex <supermftw@gmail.com>
2025-11-11 14:21:02 -0600

    Added window scaling.

--- src/main/java/me/piitex/app/App.java
+++ src/main/java/me/piitex/app/App.java
@@ -160,7 +160,7 @@ public class App extends FXLoad {
         // This is because the pathing for the image doesn't change but the image gets replaced by the new image.
         ImageLoader.useCache = false;
 
-        window = new WindowBuilder("Chat App").setIcon(new ImageLoader(new File(App.getAppDirectory(), "logo.png"))).setScale(true).setDimensions(setWidth, setHeight).build();
+        window = new WindowBuilder("Chat App").setIcon(new ImageLoader(new File(App.getAppDirectory(), "logo.png"))).setScale((appSettings.isWindowScaling()) && !mobile).setAntiAliasing(false).setDimensions(setWidth, setHeight).build();
 
         // Initialize global positions. Needed for the rendering process.
         Positions.initialize();
@@ -373,7 +373,7 @@ public class App extends FXLoad {
 
     public Window buildErrorWindow(String message) {
         if (window != null) {
-            window.close();
+            window.close(true);
         }
 
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
