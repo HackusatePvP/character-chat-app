ea5235616ab04d8c2b0afa060324c7718257bc5f
piitex <Supermftw@gmail.com>
2025-12-07 07:26:23 -0600

    Updated position values.

--- src/main/java/me/piitex/app/views/Positions.java
+++ src/main/java/me/piitex/app/views/Positions.java
@@ -43,7 +43,6 @@ public class Positions {
         Model Tabs positions
     ############################
     */
-    public static double MODEL_CONFIGURATION_SCROLL_WIDTH;
     public static double MODEL_CONFIGURATION_SCROLL_HEIGHT;
     public static double MODEL_CONFIGURATION_LAYOUT_WIDTH;
     public static double MODEL_CONFIGURATION_LAYOUT_HEIGHT;
@@ -83,9 +82,8 @@ public class Positions {
         SIDEBAR_WIDTH_COLLAPSE = 50;
         SIDEBAR_HEIGHT = (int) window.getHeight();
 
-        MODEL_CONFIGURATION_SCROLL_WIDTH = window.getWidth() - 265;
-        MODEL_CONFIGURATION_SCROLL_HEIGHT = window.getHeight() - 200;
-        MODEL_CONFIGURATION_LAYOUT_WIDTH = MODEL_CONFIGURATION_SCROLL_WIDTH - 20;
+        MODEL_CONFIGURATION_SCROLL_HEIGHT = window.getHeight() - 100;
+        MODEL_CONFIGURATION_LAYOUT_WIDTH = window.getWidth() - SIDEBAR_WIDTH - 25; // -25 to account for spacing
         MODEL_CONFIGURATION_LAYOUT_HEIGHT = -1;
         MODEL_CONFIGURATION_LAYOUT_SPACING = 10;
     }
