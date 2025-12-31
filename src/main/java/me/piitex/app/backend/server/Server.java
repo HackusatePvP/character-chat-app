1b2f83882907a630efa3b5ebb70dd7f744ef0f57
HackusatePvP <supermftw@gmail.com>
2025-12-30 19:03:28 -0600

    Temporary migrated package to the deprecated chat view.

--- src/main/java/me/piitex/app/backend/server/Server.java
+++ src/main/java/me/piitex/app/backend/server/Server.java
@@ -7,7 +7,7 @@ import me.piitex.app.backend.ChatMessage;
 import me.piitex.app.backend.Response;
 import me.piitex.app.configuration.ModelSettings;
 import me.piitex.app.utils.Placeholder;
-import me.piitex.app.views.chats.components.ReasoningLayout;
+import me.piitex.app.views.chatsdep.components.ReasoningLayout;
 import me.piitex.engine.Element;
 import me.piitex.engine.containers.CardContainer;
 import me.piitex.engine.layouts.TitledLayout;
