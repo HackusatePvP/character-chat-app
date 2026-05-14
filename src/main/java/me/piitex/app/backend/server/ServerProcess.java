c0079c31df875db6922c0a2d48c514bd2fefaf50
HackusatePvP <supermftw@gmail.com>
2026-05-14 14:15:13 -0500

    Fixed issues with new llama.cpp version where the server process would not hook start.

--- src/main/java/me/piitex/app/backend/server/ServerProcess.java
+++ src/main/java/me/piitex/app/backend/server/ServerProcess.java
@@ -315,7 +315,7 @@ public class ServerProcess {
                             process.destroy();
                             break;
                         }
-                        if (line.contains("starting the main loop")) {
+                        if (line.contains("starting the main loop") || line.contains("main: model loaded")) {
                             App.logger.info("Backend server stated!");
                             started = true;
                             break;
